ef5651cbf7568573e5875b93395b12039bd4a375
heloisacarbone <heloisacarbone@gmail.com>
2016-04-10 14:43:55 -0400

    Login and registration full OK

--- EcoMind/EcoMind/public_new/js/registration_login.js
+++ EcoMind/EcoMind/public_new/js/registration_login.js
@@ -1,3 +1,36 @@
+function login() {
+	var socket = io.connect("/");
+	var data = { 
+        action_type: "login",
+        http_type: "POST",
+        message: {
+            email: $('#loginEmail').val(),
+            password: $('#loginPassword').val(),
+        }, 
+        user_id: undefined
+                
+    };
+    
+    socket.send(JSON.stringify(data)); 
+
+    socket.on("message",function(message){  
+        
+        message = JSON.parse(message);
+        console.log(message); /*converting the data into JS object */
+        if (message.data) {
+        	document.cookie=("client_id=").concat(message.client_id);
+        
+            location.href = "home.html";
+        } else {
+            alert("There was a problem obtaining your credentials. Try again.");
+        }
+
+        $('#loginEmail').val('');
+        $('#loginPassword').val('');
+        
+    });
+}
+
 function registerUser() {
 	var socket = io.connect("/"); 
 
@@ -25,10 +58,12 @@ function registerUser() {
 
 	    socket.send(JSON.stringify(data)); 
 
-	    $('#email').val('');
-	    $('#password').val('');
-	    $('#name').val('');
-	    $('#dob').val('');
+	    $('#reg_password').val('');
+	    $('#reg_rep_password').val('');
+	    $('#reg_email').val('');
+	    $('#reg_name').val('');
+	    $('#reg_dob').val('');
+	    $("#reg_preferences input:checked").attr('checked', false)
 
 		socket.on("message",function(message){  
 	               
@@ -44,7 +79,7 @@ function registerUser() {
 	        }
 	    });
 	} else {
-		$(".error").html("Password and Repeat Password do not match");
+		$(".notice").html("Password and Repeat Password do not match");
 	}
 }
 
@@ -89,4 +124,51 @@ function createEcoInformationForm(info, userID) {
 
 function closeOverlay() {
 	$(".overlay").css("visibility", "hidden");
+}
+
+
+function submitEcoInfoForm() {
+   var socket = io.connect("/"); 
+   var responses = {
+       question1: $("#question1").val(),
+       question2: $("#question2").val(),
+       question3: $($("input[type='radio'][name='recycle_trash_radio']:checked")[0]).val(),
+       question4: $("#question4").val(),
+       question5: $("#question5").val(),
+       question6: $($("input[type='radio'][name='car_usage_radio']:checked")[0]).val(),
+       question7: $("#question7").val(),
+       question8: $("#question8").val(),
+       question9: $("#question9").val()
+   };
+
+   socket.on("message",function(response){  
+
+       response = JSON.parse(response);
+       if (response.data) {
+            $(".success").html("Your Eco-Information was succesfully saved");   
+       } else {
+            $(".error").html("We were not able to save your Eco-Information. Try it later");
+           
+       }
+       
+       $("#question1").val("");
+       $("#question2").val("");
+       $($("input[type='radio'][name='recycle_trash_radio']:checked")[0]).val("");
+       $("#question4").val("");
+       $("#question5").val("");
+       $($("input[type='radio'][name='car_usage_radio']:checked")[0]).val("");
+       $("#question7").val("");
+       $("#question8").val("");
+       $("#question9").val("");
+       $('.form').scrollTop(0);
+   });
+
+   var data = { /*creating a Js ojbect to be sent to the server*/ 
+       action_type: "submitEcoInfoForm",
+       http_type: "POST",
+       message: responses, 
+       user_id: $("#userID").html()           
+   };
+   
+   socket.send(JSON.stringify(data)); 
 }
